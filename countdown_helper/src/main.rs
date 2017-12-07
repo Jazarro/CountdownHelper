595c0fb47eb3d90fd0f2039c32c78bed9a7ff3ca
Arjan Boschman <arjan@boschman.io>
2017-12-07 12:47:35 +0100

    Rewrote most of the code, using enums now.

--- countdown_helper/src/main.rs
+++ countdown_helper/src/main.rs
@@ -1,173 +1,106 @@
 #![allow(unconditional_recursion, dead_code, unused_variables, unused_imports)]
 
+extern crate time;
+
+use time::PreciseTime;
 use std::iter;
+use std::rc::Rc;
+
+use Number::*;
 
 fn main() {
+    let start = PreciseTime::now();
+    // solve(100, vec!(1,2,3,4,20,70));
+    solve(808, prep(vec!(75, 100, 4, 1, 3, 3)));
+    // solve(527, vec!(25, 75, 8, 2, 1, 4));
     // solve(746, vec!(100, 75, 2, 10, 3, 8));
-    solve(808, vec!(75, 100, 4, 1, 3, 3));
+    // solve(24, prep(vec!(8, 4, 3, 3)));
+    // solve(900, prep(vec!(1, 2, 7, 890)));
     // solve(100, vec!(25, 75, 100, 8, 9, 10));
     // solve(100, vec!(8, 9, 10, 1));
     // solve(100, vec!(1,2,3,4,10,80));
+    let end = PreciseTime::now();
+    println!("{} seconds", start.to(end));
 }
 
-fn solve(target: usize, raw_numbers: Vec<usize>) {
-    let numbers = raw_numbers.iter()
-        .map(|number| Box::new(Atomic{value:*number}) as Box<Number>)
-        .collect::<Vec<Box<Number>>>();
-    check(target, numbers);
+fn prep(input: Vec<usize>) -> Vec<Rc<Number>> {
+    input.iter()
+        .map(|&number| Rc::new(Atomic(number)))
+        .collect()
 }
 
-fn print(numbers: &Vec<Box<Number>>) {
-    for number in numbers {
-        println!("{}", number.print());
+enum Number {
+    Atomic(usize),
+    Sum(Rc<Number>, Rc<Number>),
+    Difference(Rc<Number>, Rc<Number>),
+    Product(Rc<Number>, Rc<Number>),
+    Quotient(Rc<Number>, Rc<Number>),
+}
+
+impl Number {
+
+    fn calc(&self) -> usize {
+        match self {
+            &Atomic(value) => value,
+            &Sum(ref a, ref b) => a.calc() + b.calc(),
+            &Difference(ref a, ref b) => a.calc() - b.calc(),
+            &Product(ref a, ref b) => a.calc() * b.calc(),
+            &Quotient(ref a, ref b) => a.calc() / b.calc(),
+        }
+    }
+
+    fn print(&self) -> String {
+        match self {
+            &Atomic(ref value) => format!("{}", value),
+            &Sum(ref a, ref b) => format!("[{} + {} = {}]", a.print(), b.print(), self.calc()),
+            &Difference(ref a, ref b) => format!("[{} - {} = {}]", a.print(), b.print(), self.calc()),
+            &Product(ref a, ref b) => format!("[{} * {} = {}]", a.print(), b.print(), self.calc()),
+            &Quotient(ref a, ref b) => format!("[{} / {} = {}]", a.print(), b.print(), self.calc()),
+        }
+    }
+
+    fn calc_if_valid(&self) -> Option<usize> {
+        match self {
+            &Difference(ref a, ref b) if a.calc() < b.calc() => None,
+            &Quotient(ref a, ref b) if b.calc() == 0 => None,
+            _ => Some(self.calc()),
+        }
     }
 }
 
-fn check(target: usize, numbers: Vec<Box<Number>>) -> Vec<Box<Number>> {
-    let mut solutions: Vec<Box<Number>> = vec!();
+fn solve(target: usize, numbers: Vec<Rc<Number>>) {
     for i in 0..numbers.len() {
-        for j in 0..numbers.len() {
-            if i == j { continue; }
-            for k in 0..4 {
-                // if solutions.len() > 0 { continue; }//TODO: remove to look for more than 1 solution.
-                let a = numbers.get(i).unwrap().another();
-                let b = numbers.get(j).unwrap().another();
+        // for j in 0..numbers.len() {
+        for j in (i + 1)..numbers.len() {
+            // if i == j { continue; }
+            for k in 0..6 {
+                // if solutions.len() > 0 { continue; }//remove to look for more than 1 solution.
+                let a = Rc::clone(&numbers[i]);
+                let b = Rc::clone(&numbers[j]);
                 let combination = match k {
-                    0 => Box::new(Add{a:a,b:b}) as Box<Number>,
-                    1 => Box::new(Subtract{a:a,b:b}) as Box<Number>,
-                    2 => Box::new(Multiply{a:a,b:b}) as Box<Number>,
-                    3 => Box::new(Divide{a:a,b:b}) as Box<Number>,
+                    0 => Rc::new(Sum(a, b)),
+                    1 => Rc::new(Difference(a, b)),
+                    2 => Rc::new(Product(a, b)),
+                    3 => Rc::new(Quotient(a, b)),
+                    4 => Rc::new(Difference(b, a)),
+                    5 => Rc::new(Quotient(b, a)),
                     _ => panic!()
                 };
-                if let Some(valid_number) = combination.value() {
+                if let Some(valid_number) = combination.calc_if_valid() {
                     if valid_number == target {
                         println!("{}", combination.print());
-                        solutions.push(combination);
+                        // solutions.push(combination);
                     } else if numbers.len() > 2 {
                         let remaining_numbers = (0..numbers.len())
                             .filter(|index| index != &i && index != &j)
-                            .map(|index| numbers.get(index).unwrap().another())
+                            .map(|index| Rc::clone(&numbers[index]))
                             .chain(iter::once(combination))
-                            .collect::<Vec<Box<Number>>>();
-                        solutions.extend(check(target, remaining_numbers));
+                            .collect::<Vec<Rc<Number>>>();
+                        solve(target, remaining_numbers)
+                        // solutions.extend(check(target, remaining_numbers));
                     }
                 };
             }
         }
-    }
-    solutions
-}
-
-trait Number {
-    fn value(&self) -> Option<usize>;
-
-    fn expect(&self) -> usize {
-        self.value().unwrap()
-    }
-
-    fn another(&self) -> Box<Number>;
-
-    fn print(&self) -> String {"".to_string()}
-}
-
-struct Atomic {
-    value: usize
-}
-
-struct Add {
-    a: Box<Number>,
-    b: Box<Number>
-}
-
-struct Subtract {
-    a: Box<Number>,
-    b: Box<Number>
-}
-
-struct Multiply {
-    a: Box<Number>,
-    b: Box<Number>
-}
-
-struct Divide {
-    a: Box<Number>,
-    b: Box<Number>
-}
-
-impl Number for Atomic {
-    fn value(&self) -> Option<usize> {
-        Some(self.value)
-    }
-
-    fn another(&self) -> Box<Number> {
-        Box::new(Atomic { value: self.value })
-    }
-
-    fn print(&self) -> String {
-        format!("{}", self.value)
-    }
-}
-
-impl Number for Add {
-    fn value(&self) -> Option<usize> {
-        Some(self.a.expect() + self.b.expect())
-    }
-
-    fn another(&self) -> Box<Number> {
-        Box::new(Add { a: self.a.another(), b: self.b.another() })
-    }
-
-    fn print(&self) -> String {
-        format!("[{} + {} = {}]", self.a.print(), self.b.print(), self.expect())
-    }
-}
-
-impl Number for Subtract {
-    fn value(&self) -> Option<usize> {
-        if self.a.expect() >= self.b.expect() {
-            Some(self.a.expect() - self.b.expect())
-        } else {
-            None
-        }
-    }
-
-    fn another(&self) -> Box<Number> {
-        Box::new(Subtract { a: self.a.another(), b: self.b.another() })
-    }
-
-    fn print(&self) -> String {
-        format!("[{} - {} = {}]", self.a.print(), self.b.print(), self.expect())
-    }
-}
-
-impl Number for Multiply {
-    fn value(&self) -> Option<usize> {
-        Some(self.a.expect() * self.b.expect())
-    }
-
-    fn another(&self) -> Box<Number> {
-        Box::new(Multiply { a: self.a.another(), b: self.b.another() })
-    }
-
-    fn print(&self) -> String {
-        format!("[{} * {} = {}]", self.a.print(), self.b.print(), self.expect())
-    }
-}
-
-impl Number for Divide {
-    fn value(&self) -> Option<usize> {
-        if self.b.expect() != 0 && self.a.expect() % self.b.expect() == 0 {
-            Some(self.a.expect() / self.b.expect())
-        } else {
-            None
-        }
-    }
-
-    fn another(&self) -> Box<Number> {
-        Box::new(Divide { a: self.a.another(), b: self.b.another() })
-    }
-
-    fn print(&self) -> String {
-        format!("[{} / {} = {}]", self.a.print(), self.b.print(), self.expect())
     }
 }
