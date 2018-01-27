b9c7c1ee76b9d1f698738afec019377949bd0a6d
Arjan Boschman <arjan@boschman.io>
2018-01-27 16:15:52 +0100

    A bunch of uncommited changes from over a month ago. I think I tried to add caching to boost performance.

--- countdown_helper/src/main.rs
+++ countdown_helper/src/main.rs
@@ -2,19 +2,24 @@
 
 extern crate time;
 
-use time::PreciseTime;
+use std::collections::HashSet;
+use std::hash::Hash;
+use std::hash::Hasher;
 use std::iter;
 use std::rc::Rc;
+
+use time::PreciseTime;
 
 use Number::*;
 
 fn main() {
     let start = PreciseTime::now();
-    // solve(100, vec!(1,2,3,4,20,70));
-    solve(808, prep(vec!(75, 100, 4, 1, 3, 3)));
+    // Solver::solve(100, vec!(1,2,3,4,20,70));
+    // Solver::solve(808, vec!(75, 100, 4, 1, 3, 3));
+    Solver::solve(768, vec!(100, 7, 2, 9, 8, 4));
+    // Solver::solve(24, vec!(8, 4, 3, 3));
     // solve(527, vec!(25, 75, 8, 2, 1, 4));
     // solve(746, vec!(100, 75, 2, 10, 3, 8));
-    // solve(24, prep(vec!(8, 4, 3, 3)));
     // solve(900, prep(vec!(1, 2, 7, 890)));
     // solve(100, vec!(25, 75, 100, 8, 9, 10));
     // solve(100, vec!(8, 9, 10, 1));
@@ -23,82 +28,177 @@ fn main() {
     println!("{} seconds", start.to(end));
 }
 
-fn prep(input: Vec<usize>) -> Vec<Rc<Number>> {
-    input.iter()
-        .map(|&number| Rc::new(Atomic(number)))
-        .collect()
-}
-
+#[derive(Debug)]
 enum Number {
     Atomic(usize),
-    Sum(Rc<Number>, Rc<Number>),
-    Difference(Rc<Number>, Rc<Number>),
-    Product(Rc<Number>, Rc<Number>),
-    Quotient(Rc<Number>, Rc<Number>),
+    Sum(usize, Rc<Number>, Rc<Number>),
+    Difference(usize, Rc<Number>, Rc<Number>),
+    Product(usize, Rc<Number>, Rc<Number>),
+    Quotient(usize, Rc<Number>, Rc<Number>),
 }
 
 impl Number {
 
-    fn calc(&self) -> usize {
+    fn value(&self) -> usize {
         match self {
-            &Atomic(value) => value,
-            &Sum(ref a, ref b) => a.calc() + b.calc(),
-            &Difference(ref a, ref b) => a.calc() - b.calc(),
-            &Product(ref a, ref b) => a.calc() * b.calc(),
-            &Quotient(ref a, ref b) => a.calc() / b.calc(),
+            &Atomic(value) |
+            &Sum(value, _, _) |
+            &Difference(value, _, _) |
+            &Product(value, _, _) |
+            &Quotient(value, _, _) => value,
         }
     }
 
     fn print(&self) -> String {
         match self {
             &Atomic(ref value) => format!("{}", value),
-            &Sum(ref a, ref b) => format!("[{} + {} = {}]", a.print(), b.print(), self.calc()),
-            &Difference(ref a, ref b) => format!("[{} - {} = {}]", a.print(), b.print(), self.calc()),
-            &Product(ref a, ref b) => format!("[{} * {} = {}]", a.print(), b.print(), self.calc()),
-            &Quotient(ref a, ref b) => format!("[{} / {} = {}]", a.print(), b.print(), self.calc()),
+            &Sum(ref value, ref a, ref b) => format!("[{} + {} = {}]", a.print(), b.print(), value),
+            &Difference(ref value, ref a, ref b) => format!("[{} - {} = {}]", a.print(), b.print(), value),
+            &Product(ref value, ref a, ref b) => format!("[{} * {} = {}]", a.print(), b.print(), value),
+            &Quotient(ref value, ref a, ref b) => format!("[{} / {} = {}]", a.print(), b.print(), value),
         }
     }
 
-    fn calc_if_valid(&self) -> Option<usize> {
+    fn is_valid_difference(a: &Number, b: &Number) -> bool {
+        a.value() >= b.value()
+    }
+
+    fn is_valid_quotient(a: &Number, b: &Number) -> bool {
+        b.value() != 0 && a.value() % b.value() == 0
+    }
+
+    fn is_sensible(value: &usize, a: &Number, b: &Number) -> bool {
+        !a.num_in_history(value) && !b.num_in_history(value)
+    }
+
+    fn num_in_history(&self, num: &usize) -> bool {
         match self {
-            &Difference(ref a, ref b) if a.calc() < b.calc() => None,
-            &Quotient(ref a, ref b) if b.calc() == 0 || a.calc() % b.calc() != 0 => None,
-            _ => Some(self.calc()),
+            &Atomic(ref value) => value == num,
+            &Sum(ref value, ref a, ref b) |
+            &Difference(ref value, ref a, ref b) |
+            &Product(ref value, ref a, ref b) |
+            &Quotient(ref value, ref a, ref b) => value == num || a.num_in_history(num) || b.num_in_history(num),
+        }
+    }
+
+    fn new_sum(a: Rc<Number>, b: Rc<Number>) -> Option<Rc<Number>> {
+        let sum_value = a.value() + b.value();
+        if Number::is_sensible(&sum_value, &a, &b) {
+            Some(Rc::new(Sum(sum_value, a, b)))
+        } else {
+            None
+        }
+    }
+
+    fn new_difference(a: Rc<Number>, b: Rc<Number>) -> Option<Rc<Number>> {
+        if Number::is_valid_difference(&a, &b)
+                && Number::is_sensible(&(a.value() - b.value()), &a, &b) {
+            Some(Rc::new(Difference(a.value() - b.value(), a, b)))
+        } else {
+            None
+        }
+    }
+
+    fn new_product(a: Rc<Number>, b: Rc<Number>) -> Option<Rc<Number>> {
+        let product_value = a.value() * b.value();
+        if Number::is_sensible(&product_value, &a, &b) {
+            Some(Rc::new(Product(product_value, a, b)))
+        } else {
+            None
+        }
+    }
+
+    fn new_quotient(a: Rc<Number>, b: Rc<Number>) -> Option<Rc<Number>> {
+        if Number::is_valid_quotient(&a, &b)
+                && Number::is_sensible(&(a.value() / b.value()), &a, &b) {
+            Some(Rc::new(Quotient(a.value() / b.value(), a, b)))
+        } else {
+            None
+        }
+    }
+
+}
+
+impl Eq for Number {}
+
+impl PartialEq for Number {
+    fn eq(&self, other: &Number) -> bool {
+        match (self, other) {
+            (&Atomic(ref a), &Atomic(ref b)) => a == b,
+            (&Sum(_, ref a1, ref a2), &Sum(_, ref b1, ref b2)) => commutative(a1, a2, b1, b2),
+            (&Difference(_, ref a1, ref a2), &Difference(_, ref b1, ref b2)) => non_commutative(a1, a2, b1, b2),
+            (&Product(_, ref a1, ref a2), &Product(_, ref b1, ref b2)) => commutative(a1, a2, b1, b2),
+            (&Quotient(_, ref a1, ref a2), &Quotient(_, ref b1, ref b2)) => non_commutative(a1, a2, b1, b2),
+            _ => false,
         }
     }
 }
 
-fn solve(target: usize, numbers: Vec<Rc<Number>>) {
-    for i in 0..numbers.len() {
-        for j in (i + 1)..numbers.len() {
-            for k in 0..6 {
-                // if solutions.len() > 0 { continue; }//remove to look for more than 1 solution.
-                let a = Rc::clone(&numbers[i]);
-                let b = Rc::clone(&numbers[j]);
-                let combination = match k {
-                    0 => Rc::new(Sum(a, b)),
-                    1 => Rc::new(Difference(a, b)),
-                    2 => Rc::new(Product(a, b)),
-                    3 => Rc::new(Quotient(a, b)),
-                    4 => Rc::new(Difference(b, a)),
-                    5 => Rc::new(Quotient(b, a)),
-                    _ => panic!()
-                };
-                if let Some(valid_number) = combination.calc_if_valid() {
-                    if valid_number == target {
-                        println!("{}", combination.print());
-                        // solutions.push(combination);
-                    } else if numbers.len() > 2 {
-                        let remaining_numbers = (0..numbers.len())
-                            .filter(|index| index != &i && index != &j)
-                            .map(|index| Rc::clone(&numbers[index]))
-                            .chain(iter::once(combination))
-                            .collect::<Vec<Rc<Number>>>();
-                        solve(target, remaining_numbers);
-                        // solutions.extend(check(target, remaining_numbers));
+fn commutative(a1: &Number, a2: &Number, b1: &Number, b2: &Number) -> bool {
+    (a1 == b1 && a2 == b2) || (a1 == b2 && a2 == b1)
+}
+
+fn non_commutative(a1: &Number, a2: &Number, b1: &Number, b2: &Number) -> bool {
+    a1 == b1 && a2 == b2
+}
+
+struct Solver {
+    solutions: Vec<Rc<Number>>
+}
+
+impl Solver {
+
+    fn new() -> Solver {
+        Solver {solutions: Vec::new()}
+    }
+
+    fn solve(target: usize, numbers: Vec<usize>) {
+        println!("Solver created for target {} and numbers {:?}", target, numbers);
+        Solver::new().compute(target, Solver::prep(numbers));
+    }
+
+    fn prep(input: Vec<usize>) -> Vec<Rc<Number>> {
+        input.iter()
+            .map(|&number| Rc::new(Atomic(number)))
+            .collect()
+    }
+
+    fn compute(&mut self, target: usize, numbers: Vec<Rc<Number>>) {
+        for i in 0..numbers.len() {
+            for j in (i + 1)..numbers.len() {
+                for k in 0..6 {
+                    // if solutions.len() > 0 { continue; }//remove to look for more than 1 solution.
+                    let a = Rc::clone(&numbers[i]);
+                    let b = Rc::clone(&numbers[j]);
+                    let result_candidate = match k {
+                        0 => Number::new_sum(a, b),
+                        1 => Number::new_difference(a, b),
+                        2 => Number::new_product(a, b),
+                        3 => Number::new_quotient(a, b),
+                        4 => Number::new_difference(b, a),
+                        5 => Number::new_quotient(b, a),
+                        _ => panic!()
+                    };
+                    if let Some(result) = result_candidate {
+                        if result.value() == target {
+                            if self.solutions.iter().any(|value| value == &result) {
+                                // println!("Duplicate found!");
+                            } else {
+                                println!("{}", result.print());
+                                self.solutions.push(result);
+                            }
+                        } else if numbers.len() > 2 {
+                            let remaining_numbers = (0..numbers.len())
+                                .filter(|index| index != &i && index != &j)
+                                .map(|index| Rc::clone(&numbers[index]))
+                                .chain(iter::once(result))
+                                .collect::<Vec<Rc<Number>>>();
+                            self.compute(target, remaining_numbers);
+                        }
                     }
                 }
             }
         }
     }
+
 }
